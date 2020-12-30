b3a75452163f2d3f48b5f8264fc57cb4114fe241
Giovanni Lorenzini <giovanni@lorenzinigiovanni.com>
2020-12-30 21:04:28 +0100

    Apply perspective transform to image

--- FrameRecognition/main.py
+++ FrameRecognition/main.py
@@ -75,6 +75,44 @@ class Line():
         return (x, y)
 
 
+def order_points(pts):
+    rect = np.zeros((4, 2), dtype="float32")
+
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+
+    return rect
+
+
+def four_point_transform(image, pts):
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    return warped
+
+
 image = cv2.imread("../Images/02.png")
 
 image = imutils.resize(image, height=300)
@@ -230,5 +268,10 @@ for i in range(0, len(frameLines)-1):
 
 points = cv2.addWeighted(image, 0.8, pointsImage, 1, 0)
 cv2.imshow("points", points)
+
+pts = np.array(intersectionPoints, dtype="float32")
+dst = four_point_transform(originalImage, pts)
+
+cv2.imshow("dst", dst)
 
 cv2.waitKey(0)
