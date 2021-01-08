8136cb26e8b87bb0cf1c702aafa308f83d4268c4
Diego Planchenstainer <diego.plank@gmail.com>
2021-01-08 21:08:39 +0100

    Add function to convert rgb to hsv

--- utils.py
+++ utils.py
@@ -7,6 +7,30 @@ def hsv2rgb(h,  s,  v):
     return tuple(round(i * 255) for i in colorsys.hsv_to_rgb(h,  s,  v))
 
 
+def rgb2hsv(r, g, b):
+    r = float(r) / 255
+    g = float(g) / 255
+    b = float(b) / 255
+    high = max(r, g, b)
+    low = min(r, g, b)
+    h, s, v = high, high, high
+
+    d = high - low
+    s = 0 if high == 0 else d/high
+
+    if high == low:
+        h = 0.0
+    else:
+        h = {
+            r: (g - b) / d + (6 if g < b else 0),
+            g: (b - r) / d + 2,
+            b: (r - g) / d + 4,
+        }[high]
+        h /= 6
+
+    return h, s, v
+
+
 def getColorMean(image):
     mask = np.ones([image.shape[0], image.shape[1]], np.uint8)
     mean = cv2.mean(image, mask=mask)
