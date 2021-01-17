1a6fdc5cf2516d98adb21749c7c19790501c9d6d
Simone Luchetta <simo_lucketts@outlook.it>
2021-01-17 22:29:23 +0100

    Add overlay of results

--- main.py
+++ main.py
@@ -95,4 +95,38 @@ histogramDiscrimination = recognitionByHistogram(
     sixths, beeHistogram, openCellHistogram, closedCellHistogram)
 print(histogramDiscrimination)
 
+overlay = warped.copy() * 0
+
+counter = 0
+for yy in range(0, 2):
+    for xx in range(0, 3):
+        x = warped.shape[1]
+        y = warped.shape[0]
+
+        xStart = round(xx * (x / 3))
+        yStart = round(yy * (y / 2))
+        xEnd = round((1 + xx) * (x / 3))
+        yEnd = round((1 + yy) * (y / 2))
+
+        color = (0, 0, 0)
+
+        if (histogramDiscrimination[counter] == "Bee"):
+            color = (255, 0, 0)
+        elif (histogramDiscrimination[counter] == "Open"):
+            color = (0, 255, 0)
+        elif (histogramDiscrimination[counter] == "Close"):
+            color = (0, 0, 255)
+
+        cv2.rectangle(overlay, (xStart, yStart),
+                      (xEnd, yEnd), color, -1)
+        cv2.rectangle(overlay, (xStart, yStart),
+                      (xEnd, yEnd), (255, 255, 255), 2)
+        cv2.putText(
+            overlay, histogramDiscrimination[counter], (xStart + 10, yEnd - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+
+        counter += 1
+
+res = cv2.addWeighted(warped, 0.7, overlay, 0.3, 1.0)
+cv2.imshow("Result", res)
+
 cv2.waitKey(0)
